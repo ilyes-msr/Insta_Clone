1d9a505baf03a002df62c77b558a762cd6dc0aa3
Ali Ilyes MANSOUR <mansour.aliilyes@gmail.com>
2024-07-01 12:03:27 +0100

    Build models and seed data using Model Factories

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $images = ['pic1.jpeg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg'];
+
+        return [
+            'description' => fake()->sentence(),
+            'slug' => fake()->regexify('[A-Za-z0-9]{10}'),
+            'user_id' => User::factory(),
+            'image' => 'posts/' . fake()->randomElement($images)
+        ];
+    }
+}
