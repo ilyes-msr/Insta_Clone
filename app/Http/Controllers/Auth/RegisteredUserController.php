484901ab3bb42f3efa8fb3fc05c96187ea0cd1c1
Ali Ilyes MANSOUR <mansour.aliilyes@gmail.com>
2024-07-01 10:48:05 +0100

    Customize registration page

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -32,12 +32,15 @@ class RegisteredUserController extends Controller
     {
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
+            'username' => ['required', 'string', 'max:100', 'unique:users', 'alpha_dash'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
         $user = User::create([
             'name' => $request->name,
+            'username' => $request->username,
+            'image' => 'https://ui-avatars.com/api/?name=' . urlencode($request->username),
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
